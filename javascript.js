3586bbd9abef32d0cf70c39c4ce2c86028591eb2
Jancarlos Aguasvivas <aguasvivasjc@gmail.com>
2022-01-24 22:02:02 -0500

    Save Button & Console Log Activation

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
 // moment.j -- time & date (local)
+var dateEl = document.getElementById("currentDay");
 var dateElement = document.querySelector("#currentDay");
 var today = moment();
+const description = document.querySelector(".description");
+const saveBtn = document.querySelectorAll(".saveBtn");
+console.log(saveBtn)
+
+
+
 // $("#currentDay").text(today.format("MM Do, YYYY"));
-dateElement.textContent = today
 
 
 // save entry to local storage using save button
 
-var dateEl = document.getElementById("currentDay");
 
-function TimeUpdate () {
+function timeUpdate() {
     var today = moment().format("MMM DD, YYYY [at] hh:mm:ss A");
     $("#currentDay").text(today);
     dateEl.textContent = today;
 }
-setInterval(TimeUpdate, 1000);
+
+function save(element, saveName) {
+    localStorage.setItem(saveName, element.value);
+}
+
+
+
+dateElement.textContent = today
+setInterval(timeUpdate, 1000);
+$(".container").on('click' , ".saveBtn", function(event){
+console.log($(this).parent().attr("id"))
+});
