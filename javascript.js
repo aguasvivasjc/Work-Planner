12883c66483c025c804b5322b4b7d16ce9fb6394
Jancarlos Aguasvivas <aguasvivasjc@gmail.com>
2022-01-25 21:08:22 -0500

    Time Block Entry Save

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-// moment.j -- time & date (local)
 var dateEl = document.getElementById("currentDay");
 var dateElement = document.querySelector("#currentDay");
 var today = moment();
@@ -6,28 +5,44 @@ const description = document.querySelector(".description");
 const saveBtn = document.querySelectorAll(".saveBtn");
 console.log(saveBtn)
 
+// moment.j -- time & date (local)
 
-
-// $("#currentDay").text(today.format("MM Do, YYYY"));
-
+function timeUpdate() {
+    dateEl.textContent = moment().format("MMM DD, YYYY [at] hh:mm:ss A");
+    // $("#currentDay").text(today);
+    // dateEl.textContent = today;
+}
 
 // save entry to local storage using save button
 
-
-function timeUpdate() {
-    var today = moment().format("MMM DD, YYYY [at] hh:mm:ss A");
-    $("#currentDay").text(today);
-    dateEl.textContent = today;
-}
-
 function save(element, saveName) {
     localStorage.setItem(saveName, element.value);
+
 }
 
+// time comparions function for past & present 
 
 
-dateElement.textContent = today
+
+// dateElement.textContent = today
 setInterval(timeUpdate, 1000);
-$(".container").on('click' , ".saveBtn", function(event){
-console.log($(this).parent().attr("id"))
+
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+    // $(".container").on('click' , ".saveBtn", function(event){
+    // console.log($(this).parent().attr("id"))
+    // console.log($(this.previousElementSibling).val())
+    let hour = $(this).parent().attr("id")
+    let description = ($(this.previousElementSibling).val())
+    console.log(hour);
+    console.log(description);
+    localStorage.setItem(hour, description);
+    
 });
+
+$("#9am .description").val(localStorage.getItem("9am"));
+
+
+
+
+// $("#currentDay").text(today.format("MM Do, YYYY"));
